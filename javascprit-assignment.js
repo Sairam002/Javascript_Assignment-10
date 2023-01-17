8046936652bfc823f5ebe3dd0cb005ec519a2f23
Sairam002 <106730092+Sairam002@users.noreply.github.com>
2023-01-17 15:44:14 +0530

    Add files via upload

---
+++ javascprit-assignment.js
@@ -0,0 +1,20 @@
+//1
+"No Higher order functions and callback functions are both different."
+"In higher order functions the input parameter will be a function which means A function is given as a input"
+" whereas in callback function the input parameter will be a datatype like string, number, array etc."
+
+//2
+"Filter is an inbuilt higher order function because it takes the input as a parameter to the function and process the function."
+
+//4
+    //a
+    "Hello John",
+    "Hello Tena",
+    "Hello Kale",
+    "Hello max"
+
+    //b
+    "UseFunction is the higher order function and argFn is callback function"
+
+//3
+"The example which is provided in the 4th question is an example for higher order function and callback function."
